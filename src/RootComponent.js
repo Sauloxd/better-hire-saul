acd306119997109d51924b435d7bc188c02ea66d
Sauloxd <saulotoshi@gmail.com>
2017-03-30 20:15:59 -0300

    sidebar click and go, done

--- src/RootComponent.js
+++ src/RootComponent.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router,} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Sidebar, Content } from './components'
 import globalStyl from './theme/theme.styl'
+import createBrowserHistory from 'history/createBrowserHistory'
 
 export default class RootComponent extends React.Component {
 	render() {
+		const history = createBrowserHistory()
 		return (
-			<Router>
-				<div style={{ display: 'flex', height: '100%' }}>
+      <Router >
+        <div style={{ display: 'flex', height: '100%' }}>
           <Sidebar />
           <Content />
-			  </div>
-		  </Router>
+        </div>
+      </Router>
 		);
 	}
 }
--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,12 +2,14 @@ import React from 'react';
 import contentStyles from './Content.styl';
 import {
   Route,
-  Link
+  Link,
+	Redirect
 } from 'react-router-dom';
 import routes from '../../routes.js';
 
 const Content = () => (
   <div style={{ flex: 1, padding: '10px' }}>
+  	<Route exact path="/" render={() => <Redirect to="/me/who-am-i" />} />
     {routes.map((route, index) => (
     <Route
       key={index}
